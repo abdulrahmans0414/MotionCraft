d744f7160a01007ce4ddb4de9dc72abf00b6c6f1
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-03-16 14:57:34 +0530

    update AnimationsPage to display all animation components

--- src/Pages/AnimationsPage.jsx
+++ src/Pages/AnimationsPage.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import BasicAnimations from "../components/Animations/BasicAnimations";
+import KeyframeAnimations from "../components/Animations/KeyframeAnimations";
+import GestureAnimations from "../components/Animations/GestureAnimations";
+import VariantsAnimations from "../components/Animations/VariantsAnimations";
 
 function AnimationsPage() {
-    return <div>AnimationsPage</div>;
+    return (
+        <div className="bg-[#222]  text-white min-h-screen p-5">
+            <h1 className="text-white text-center text-4xl font-bold mb-10">
+                Framer Motion Animations
+            </h1>
+
+            <section className="mb-10">
+                <h2 className="text-white text-3xl font-semibold mb-5">Basic Animations</h2>
+                <BasicAnimations />
+            </section>
+
+            <section className="mb-10">
+                <h2 className="text-white text-3xl font-semibold mb-5">Keyframe Animations</h2>
+                <KeyframeAnimations />
+            </section>
+
+            <section className="mb-10">
+                <h2 className="text-white text-3xl font-semibold mb-5">Gesture Animations</h2>
+                <GestureAnimations />
+            </section>
+
+            <section className="mb-10">
+                <h2 className="text-white text-3xl font-semibold mb-5">Variants Animations</h2>
+                <VariantsAnimations />
+            </section>
+        </div>
+    )
 }
 
 export default AnimationsPage;
