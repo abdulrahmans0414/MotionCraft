3125d80dfbf3521de037a538f946f530f230ec06
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-03-16 14:57:55 +0530

    update ScrollAnimationPage for scroll-based animations

--- src/Pages/ScrollAnimationPage.jsx
+++ src/Pages/ScrollAnimationPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ScrollAnimations from "../components/Animations/ScrollAnimations";
 
 function ScrollAnimationPage() {
-    return <div>ScrollAnimationPage</div>;
+    return (
+        <div className="bg-[#222] text-white min-h-screen">
+            <ScrollAnimations />
+        </div>
+    )
 }
 
 export default ScrollAnimationPage;
